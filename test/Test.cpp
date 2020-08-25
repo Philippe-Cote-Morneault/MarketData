8bfb2c4002efc4eb54470fc8b2cbf5c9a045c212
Philippe Cote-Morneault <philippe.cote.11@gmail.com>
2020-08-24 21:19:43 -0400

    Added some tests for new features.

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <time.h>
+#include <vector>
+
 #include "../include/PythonFinance.hpp"
+#include "../include/Nasdaq.hpp"
 
 int main() {
-    std::cout << python_finance::getSymbolChart("MSFT", 1564617600, 1596672000) << std::endl;
+    yahoo_finance::getSymbolChart("MSFT", 1564617600, 1596672000);
+    auto j = yahoo_finance::getSymbolSummary("MSFT");
+    std::cout << j["context"]["dispatcher"]["stores"]["QuoteSummaryStore"] .dump(4) << std::endl;
+
+    std::vector<yahoo_finance::nasdaq::RealTimeTrade> trades = yahoo_finance::nasdaq::getRealTimeTrades("MSFT", "15:00");
+
+    for (int i = 0; i < trades.size(); ++i) {
+        std::cout << trades[i].price << std::endl;
+    }
     return 0;
 }
