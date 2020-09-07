cb1b2e7db4054585de8fdc6f565be657ddca1bcc
Philippe Cote-Morneault <philippe.cote.11@gmail.com>
2020-09-06 21:46:34 -0400

    Change cURL write callback to be static.

--- market_data/nasdaq/nasdaq.hpp
+++ market_data/nasdaq/nasdaq.hpp
@@ -28,7 +28,7 @@ namespace market_data {
             std::string formatUrl(const std::string &symbol, const std::string &time, int limit);
         }
 
-        size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+        static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
             ((std::string*)userp)->append((char*)contents, size * nmemb);
             return size * nmemb;
         }
--- market_data/yahoo/yahoo.hpp
+++ market_data/yahoo/yahoo.hpp
@@ -14,7 +14,7 @@ namespace market_data {
             std::string formatUrl(const std::string &symbol, long start, long end);
         }
 
-        size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+        static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
             ((std::string *) userp)->append((char *) contents, size * nmemb);
             return size * nmemb;
         }
